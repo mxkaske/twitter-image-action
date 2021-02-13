1909436b3b2c5fc3065917af75f00b37e538bba1
Maximilian Kaske <maximilian@kaske.org>
2021-02-13 18:01:20 +0100

    :art: add basic colors

---
+++ constants.js
@@ -0,0 +1,11 @@
+const backgroundColors = {
+  ligth: "#fdfffc",
+  dark: "#011627",
+};
+
+const colors = {
+  indigo: "#4f46e5",
+  yellow: "#fde047",
+};
+
+module.exports = { colors, backgroundColors };
