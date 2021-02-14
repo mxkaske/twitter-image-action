cf65791c49e0a06bbb6913555d03112dc65a8b6c
Maximilian Kaske <maximilian@kaske.org>
2021-02-14 13:35:58 +0100

    :art: add more colors

--- constants.js
+++ constants.js
@@ -1,11 +1,19 @@
 const backgroundColors = {
-  light: "#fdfffc",
-  dark: "#011627",
+  light: "#E0F2FE",
+  dark: "#1C1917",
 };
 
 const colors = {
   indigo: "#4f46e5",
   yellow: "#fde047",
+  teal: "#0D9488",
+  pink: "#DB2777",
+  green: "#16A34A",
+  purple: "#9333EA",
+  orange: "#EA580C",
+  red: "#DC2626",
+  lime: "#65A30D",
+  lightBlue: "#0284C7",
 };
 
 module.exports = { colors, backgroundColors };
