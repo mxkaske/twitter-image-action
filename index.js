8b7cd1a54183c5176f422418b1d347ce2d20abd5
Maximilian Kaske <maximilian@kaske.org>
2021-02-14 13:16:54 +0100

    :construction_worker: get weather infos

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ dotenv.config();
 const Twitter = require("twitter");
 const sharp = require("sharp");
 const fs = require("fs");
+const weather = require("openweather-apis");
 const { colors, backgroundColors } = require("./constants");
+
+const currentUTC = Math.round(Date.now() / 1000);
 
 const client = new Twitter({
   consumer_key: process.env.apikey,
@@ -11,6 +14,21 @@ const client = new Twitter({
   access_token_key: process.env.accesstoken,
   access_token_secret: process.env.accesstokensecret,
 });
+
+// region get weather data
+weather.setCity("Berlin");
+weather.setAPPID(process.env.weatherapikey);
+weather.getAllWeather(function (err, JSONObj) {
+  // console.log(JSONObj);
+  const {
+    sys: { sunrise, sunset },
+    clouds: { all },
+  } = JSONObj;
+  const light = sunrise < currentUTC && currentUTC < sunset;
+  const clear = all < 20;
+  console.log({ light, clear });
+});
+//endregion
 
 // region manipulate svg
 const glasses = `.st13{fill:${colors.yellow};stroke:#000000;stroke-miterlimit:10;}`;
